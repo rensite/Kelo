0bc8e449727e9bb2d2935cc2c43fb0331c0a2227
Renatto Galini <renat@Renattos-iMac.local>
2020-05-04 01:22:13 +0500

    +purge css, thin fonts

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,20 @@
         <div v-if="(taskList.length > 0)">
           <div v-if="totalTasks === 0" class="flex flex-col items-center justify-center text-center text-gray-500 py-16">
             <font-awesome-icon icon="check-double" class="mb-5 text-6xl" />
-            <span class="text-lg">All tasks had been completed! ツ</span>
+            <span class="text-lg font-light">All tasks had been completed! ツ</span>
           </div>
           <div v-for="task in taskList" :key="task.id">
             <TaskRow :task="task" @updateTask="updateTask" @removeTask="removeTask"></TaskRow>
           </div>
         </div>
-        <div v-else class="flex flex-col items-center justify-center text-center text-gray-500 text-lg py-16">  
+        <div v-else class="flex flex-col items-center font-light justify-center text-center text-gray-500 text-lg py-16">  
           <font-awesome-icon icon="star" class="mb-5 text-6xl" />
           <span>There are no tasks yet.<br> It's perfect time to start!</span>
         </div>
       </div>
     </div>
     <footer class="text-center pt-6 pb-16">
-      <span class="text-sm text-gray-300 mr-2">&copy; 2020 Renat Galin</span>
+      <span class="text-sm text-gray-300 mr-2 font-light">&copy; 2020 Renat Galin</span>
       <a href="https://github.com/rensite/Kelo" class="text-gray-300 hover:text-gray-600" target="_blank">
         <font-awesome-icon :icon="['fab', 'github']" prefix="fab" class="text-lg" />
       </a>
